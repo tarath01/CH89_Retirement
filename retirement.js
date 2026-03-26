c2956191b6f921bf32b47f85609cc286f9e8dacf
tarath01 <epstrath01@gmail.com>
2026-03-26 13:53:03 -0500

    Validations

--- retirement.js
+++ retirement.js
@@ -1,19 +1,28 @@
+/*******************************
+Name: Taylor Rath
+Program: Retirement Countdown
+Date: 03/24/2026
+Program Description:
+
+*******************************/
+
+
 "use strict";
 
 // Defer in HTML allows us to grab these immediately at the top
-const $ = selector => document.querySelector(selector);
+const getElement = selector => document.querySelector(selector);
 
-const nameIn    = $("#client_name");
-const emailIn   = $("#email");
-const investIn  = $("#investment");
-const addIn     = $("#monthly_add");
-const rateIn    = $("#rate");
-const dateIn    = $("#retirement_date");
-const errBox    = $("#error_message");
-const statusMsg = $("#status_message");
-const output    = $("#projection_output");
-const form      = $("#projection_form");
-const testData  = $("#test_data");
+const nameIn    = getElement("#client_name");
+const emailIn   = getElement("#email");
+const investIn  = getElement("#investment");
+const addIn     = getElement("#monthly_add");
+const rateIn    = getElement("#rate");
+const dateIn    = getElement("#retirement_date");
+const errBox    = getElement("#error_message");
+const statusMsg = getElement("#status_message");
+const output    = getElement("#projection_output");
+const form      = getElement("#projection_form");
+const testData  = getElement("#test_data");
 
 let projectionTimer = null;
 
@@ -24,21 +33,58 @@ const formatter = new Intl.NumberFormat('en-US', {
     maximumFractionDigits: 2
 });
 
+//error message fields
+const nameErr = document.querySelector("#name_error");
+const emailErr = document.querySelector("#email_error");
+const investmentErr = document.querySelector("#investment_error");
+const rateErr = document.querySelector("#rate_error");
+const addErr = document.querySelector("#add_error");
+const retireErr = document.querySelector("#retire_date_error");
+
+
 const processEntries = (evt) => {
     let isValid = true;
     let years = 0;
 
+    //prevent form from being submitted to server
     evt.preventDefault();
     resetForm()
 
+    const name = nameIn.value.trim();
+    const email = emailIn.value.trim();
+    const investment = parseInt(investIn.value);
+    const add = parseFloat(addIn.value);
+    const rate = parseFloat(rateIn.value);
+
     // TODO: Validate Name
-
-    // TODO: Validate Email
-
+if (name === "") {
+    nameErr.textContent = "Please enter your full name";
+    isValid = false;
+}
+    // TODO: Validate Email - const emailPattern = /^[\w\.\-]+@[\w\.\-]+\.[a-zA-Z]+$/;
+const emailPattern = /^[\w\.\-]+@[\w\.\-]+\.[a-zA-Z]+$/;
+if (!emailPattern.test(email)) {
+    emailErr.textContent = "Enter your valid college email address.";
+    isValid = false;
+}
     // TODO: Validate Date
-
+if (years <=0) {
+    retireErr.textContent = "Greater than 0 & less than 75 years";
+    isValid = false;
+}
     // TODO: Numeric Validations
-
+if (isNaN(rate) || rate < 0) {
+    rateErr.textContent = "Annual interest rate, not less than 0 or greater than 20";
+    isValid = false;
+}
+if (isNaN(add) || add <= 0) {
+    addErr.textContent = "How much you add each month, not less than 0.";
+    isValid = false;
+}
+    if (isNaN(investment) || investment < 0) {
+        investmentErr.textContent = "Current Savings total, not less than 0";
+        isValid = false;
+}
     /* TODO: Code try-catch logic
         try
             if not valid then throw error "Please correct the entries highlighted below."
@@ -48,6 +94,16 @@ const processEntries = (evt) => {
             set the body width to 700px (like code above)
             errBox.innerText = e.message;
      */
+    try {
+        if (!isValid) {
+            new Error("Please correct the entries highlighted below.");
+        }
+        document.body.style.width = "350px";
+        startProjection(nameIn.value, investIn.value, addIn.value, rateIn.value, years);
+    } catch (e) {
+        document.body.style.width = "700px";
+        errBox.textContent = e.message;
+    }
 };
 
 const startProjection = (name, bal, add, rate, years) => {
@@ -60,6 +116,15 @@ const startProjection = (name, bal, add, rate, years) => {
 
     let formattedBal = formatter.format(bal);
     output.innerHTML = `Year ${startYear} = ${formattedBal}`;
+
+    for (let i = 0; i < 12; i++) {
+        bal = (bal + add) * (1 + (rate / 12 / 100)).toFixed(2);
+    }
+
+    output.innerHTML += `${formattedBal}`;
+    if (count >= years) {
+
+    }
 
     /* TODO: setup an interval to do the following
         for (let i = 0; i < 12; i++) {
@@ -89,7 +154,7 @@ const resetForm = () => {
         set the body width to 350px (like code above)
         set the focus to the name input field
      */
-};
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     form.addEventListener("submit", processEntries);
